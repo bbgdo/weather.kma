26e546dfe89cd1c5809003f04325f11a9d2665cb
chucho <bohdan.tarverdiiev@gmail.com>
2025-10-31 13:30:06 +0200

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import unusedImports from 'eslint-plugin-unused-imports'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,57 +15,19 @@ export default defineConfig([
             tseslint.configs.recommended,
             reactHooks.configs['recommended-latest'],
             reactRefresh.configs.vite,
-            'plugin:react/recommended',
-            'plugin:jsx-a11y/recommended',
-            'plugin:import/recommended',
-            'plugin:prettier/recommended',
+            unusedImports.configs.recommended,
         ],
         languageOptions: {
             ecmaVersion: 2022,
             sourceType: 'module',
             globals: globals.browser,
         },
-        settings: {
-            react: { version: 'detect' },
-        },
         rules: {
-            '@typescript-eslint/no-explicit-any': 'warn',
-            '@typescript-eslint/explicit-function-return-type': 'off',
             '@typescript-eslint/no-unused-vars': 'off',
             'unused-imports/no-unused-imports': 'error',
 
-            'react/react-in-jsx-scope': 'off',
-            'react/prop-types': 'off',
-            'react/jsx-uses-react': 'off',
-            'react/jsx-uses-vars': 'error',
-
             'react-hooks/rules-of-hooks': 'error',
             'react-hooks/exhaustive-deps': 'warn',
-
-            'import/order': [
-                'warn',
-                {
-                    groups: [
-                        'builtin',
-                        'external',
-                        'internal',
-                        ['parent', 'sibling', 'index'],
-                    ],
-                    'newlines-between': 'always',
-                },
-            ],
-            'import/no-unresolved': 'off',
-            'import/newline-after-import': ['warn', { count: 1 }],
-
-            'prettier/prettier': [
-                'warn',
-                {
-                    singleQuote: true,
-                    semi: false,
-                    trailingComma: 'all',
-                    endOfLine: 'auto',
-                },
-            ],
         },
     },
 ])
