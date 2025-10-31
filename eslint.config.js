829c4df2843b5a1444390b616ab71a6e210efe69
chucho <bohdan.tarverdiiev@gmail.com>
2025-10-31 13:27:20 +0200

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,18 +6,65 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+    globalIgnores(['dist', 'node_modules']),
+    {
+        files: ['**/*.{ts,tsx}'],
+        extends: [
+            js.configs.recommended,
+            tseslint.configs.recommended,
+            reactHooks.configs['recommended-latest'],
+            reactRefresh.configs.vite,
+            'plugin:react/recommended',
+            'plugin:jsx-a11y/recommended',
+            'plugin:import/recommended',
+            'plugin:prettier/recommended',
+        ],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: globals.browser,
+        },
+        settings: {
+            react: { version: 'detect' },
+        },
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'warn',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-unused-vars': 'off',
+            'unused-imports/no-unused-imports': 'error',
+
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+            'react/jsx-uses-react': 'off',
+            'react/jsx-uses-vars': 'error',
+
+            'react-hooks/rules-of-hooks': 'error',
+            'react-hooks/exhaustive-deps': 'warn',
+
+            'import/order': [
+                'warn',
+                {
+                    groups: [
+                        'builtin',
+                        'external',
+                        'internal',
+                        ['parent', 'sibling', 'index'],
+                    ],
+                    'newlines-between': 'always',
+                },
+            ],
+            'import/no-unresolved': 'off',
+            'import/newline-after-import': ['warn', { count: 1 }],
+
+            'prettier/prettier': [
+                'warn',
+                {
+                    singleQuote: true,
+                    semi: false,
+                    trailingComma: 'all',
+                    endOfLine: 'auto',
+                },
+            ],
+        },
     },
-  },
 ])
